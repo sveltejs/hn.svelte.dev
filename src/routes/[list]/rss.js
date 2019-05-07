f8852125d95a3d1b70260fc7f67918fcb822d476
Richard Harris <richard.a.harris@gmail.com>
2019-05-07 08:45:41 -0400

    fix RSS feed

--- src/routes/[list]/rss.js
+++ src/routes/[list]/rss.js
@@ -1,23 +1,24 @@
 import fetch from 'node-fetch';
+import send from '@polka/send';
 
 const render = (list, items) => `<?xml version="1.0" encoding="UTF-8" ?>
 <rss version="2.0">
 <channel>
 	<title>Svelte HN (${list})</title>
-	<link>https://hn.svelte.technology/${list}/1</link>
+	<link>https://hn.svelte.dev/${list}/1</link>
 	<description>Links from the orange site</description>
 	<image>
-		<url>https://hn.svelte.technology/favicon.png</url>
+		<url>https://hn.svelte.dev/favicon.png</url>
 		<title>Svelte HN (${list})</title>
-		<link>https://hn.svelte.technology/${list}/1</link>
+		<link>https://hn.svelte.dev/${list}/1</link>
 	</image>
 	${items.map(item => `
 		<item>
 			<title>${item.title}${item.domain ? ` (${item.domain})` : ''}</title>
-			<link>https://hn.svelte.technology/item/${item.id}</link>
+			<link>https://hn.svelte.dev/item/${item.id}</link>
 			<description><![CDATA[${
 				item.url ? `<a href="${item.url}">link</a> / ` : ''
-				}<a href="https://hn.svelte.technology/item/${item.id}">comments</a>
+				}<a href="https://hn.svelte.dev/item/${item.id}">comments</a>
 			]]></description>
 			<pubDate>${new Date(item.time * 1000).toUTCString()}</pubDate>
 		</item>
@@ -32,15 +33,13 @@ export function get(req, res) {
 		req.params.list
 	);
 
-	res.set({
-		'Cache-Control': `max-age=0, s-max-age=${600}`, // 10 minutes
-		'Content-Type': 'application/rss+xml'
-	});
-
 	fetch(`https://api.hnpwa.com/v0/${list}/1.json`)
 		.then(r => r.json())
 		.then(items => {
 			const feed = render(list, items);
-			res.end(feed);
+			send(res, 200, feed, {
+				'Cache-Control': `max-age=0, s-max-age=${600}`, // 10 minutes
+				'Content-Type': 'application/rss+xml'
+			});
 		});
 }
--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 import sirv from 'sirv';
 import polka from 'polka';
-import compression from 'compression';
 import * as sapper from '@sapper/server';
 
 const { PORT, NODE_ENV } = process.env;
@@ -8,7 +7,6 @@ const dev = NODE_ENV === 'development';
 
 polka() // You can also use Express
 	.use(
-		compression({ threshold: 0 }),
 		sirv('static', { dev }),
 		sapper.middleware()
 	)
